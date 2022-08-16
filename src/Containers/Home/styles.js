9a548405603cef7d16986049ac5a3aac9e33d541
ana claudia <anacavalcanted@gmail.com>
2022-08-16 16:47:07 -0300

    commit project react

---
+++ src/Containers/Home/styles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components"
+import Background from "../../assets/background1.png"
+
+export const Container = styled.div`
+   background: url("${Background}") ;
+   background-size: cover ;
+   display: flex ;
+   flex-direction: column ;
+   align-items: center ;
+   gap: 40px ;
+`
+
+export const Image = styled.img`
+   margin-top:30px ;
+
+`
+
+export const InputLabel = styled.p`
+   letter-spacing: -0ch.408px ;
+   font-style:normal ;
+   font-weight: bold ;
+   font-size:18px ;
+   line-height: 22px ;
+   color: #FFFFFF;
+   margin-left: 25px ;
+`
+
+export const Input = styled.input`
+   background: rgba(255, 255, 255, 0.25);
+   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+   border-radius: 14px;
+   width:342px ;
+   height: 58px;
+   padding-left: 25px ;
+   margin-bottom: 34px;
+   border: none;
+   outline: none ;
+   font-style: normal ;
+   font-weight: normal ;
+   font-size: 20px ;
+   line-height: 20px;
+   color:  #FFFFFF;
+`
+
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+
+import Home from "./Containers/Home"
+import Users from "./Containers/Users"
+
+function MyRoutes(){
+    return(
+        <Router>
+        <Routes>
+           
+            <Route path="/" element={<Home/>}/>
+            <Route path="/usuarios" element={<Users/>} />
+
+        </Routes>
+        </Router>
+        
+    )
+}
+
+export default MyRoutes
+ 
